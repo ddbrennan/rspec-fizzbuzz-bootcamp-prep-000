bb4619df86fd6f8ffc4f91d94e7883d636adf1d1
David Brennan <ddbren@gmail.com>
2017-10-23 22:59:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
   result = nil
   if int % 3 == 0
-    result = result + "Fizz"
+    result = result.toString + "Fizz"
   end
   if int % 5 == 0
-    result = result + "Buzz"
+    result = result.toString + "Buzz"
   end
   result
 end
