dccb36c3ca52bfd16de2a364e865ec359c2c6fc9
David Brennan <ddbren@gmail.com>
2017-10-23 23:12:08 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0 && !(int % 5 == 0)
     "Fizz"
-  end
 
-  elsif int % 5 == 0 && !(int % 3 == 0)
+  elsif (int % 5 == 0) && !(int % 3 == 0)
     "Buzz"
-  end
 
   elsif (int % 5 == 0) && (int % 3 == 0)
     "FizzBuzz"
